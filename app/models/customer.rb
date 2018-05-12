2b2d97eca79ae4310af062a82747867153116995
jo war <wargasia@op.pl>
2018-05-12 11:23:43 +0200

    2 wersja modelu Customer (validates, has many)

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,2 +1,7 @@
 class Customer < ApplicationRecord
+  validates :name, presence: true
+  VALID_EMAIL_REGEX = /@/
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
+
+  has_many :offers
 end
