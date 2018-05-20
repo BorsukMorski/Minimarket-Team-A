41737d17a936194e0593cb7f384ea0d459b9dd58
Kasia <pani.dj@gmail.com>
2018-05-20 13:06:26 +0200

    has secure password

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -4,4 +4,7 @@ class Customer < ApplicationRecord
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
 
   has_many :offers
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }
+
 end
