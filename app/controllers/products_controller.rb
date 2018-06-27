7f19e532a0e91640bcfd45c020cda105badd9d64
jo war <wargasia@op.pl>
2018-06-27 17:30:29 +0200

    Dodano zmiany Aneta_Products bez resources

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,4 +2,19 @@ class ProductsController < ApplicationController
   def new
     @product = Product.new
   end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      session[:product_id] = @product.id
+      redirect_to products_path
+    else
+      redirect_to product_new_path
+    end
+  end
+
+private
+  def product_params
+    params.require(:product).permit(:author, :title, :isbn)
+  end
 end
